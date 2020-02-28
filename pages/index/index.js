f20eb11f1239ee45b519f3e1a753f3a03ee92f18
Jeny <853297695@qq.com>
2020-02-28 13:24:48 +0800

    feat:完成首页的搜索 轮播图 导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,40 @@
-//index.js
-//获取应用实例
-const app = getApp()
+import request from '../../utils/request.js'
 
 Page({
   data: {
-   
+    banners: [],
+    navs:[]
   },
-  onLoad: function () {
-   
+  onLoad: function() {
+    // 请求轮播图
+    request({
+      url: '/home/swiperdata'
+    }).then(res => {
+      // console.log(res)
+      const {
+        message
+      } = res.data
+      this.setData({
+        banners: message
+      })
+    })
+      
+      // 请求nav图
+      request({
+        url: '/home/catitems',
+      }).then(res=> {
+        console.log(res)
+        const {message} = res.data
+        // 添加跳转路径
+       let navs= message.map((v,i)=> {
+         if(v.name === "分类"){
+           v.url = "/pages/Category/index"
+         }
+         return v
+        })
+        this.setData({
+          navs
+        })
+      })
   }
 })
