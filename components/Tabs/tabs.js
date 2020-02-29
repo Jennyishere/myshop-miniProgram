ec11c172f17287309d2147028f62c3d8cf77162c
Jeny <853297695@qq.com>
2020-03-01 01:18:51 +0800

    feat:商品列表页静态 及初步数据渲染  上拉加载 下拉刷新未完成

---
+++ components/Tabs/tabs.js
@@ -0,0 +1,32 @@
+// components/Tabs/tabs.js
+Component({
+ /**
+  * 组件的属性列表
+  */
+ properties: {
+  tabs: {
+   type: Array,
+   value: []
+  }
+ },
+
+ /**
+  * 组件的初始数据
+  */
+ data: {
+
+ },
+
+ /**
+  * 组件的方法列表
+  */
+ methods: {
+  handleItemTap(e) {
+   // 获取点击索引
+const {index} = e.currentTarget.dataset;
+// 发给父组件
+this.triggerEvent("tabItemChange",{index})
+
+  }
+ }
+})
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,18 +1,66 @@
 // pages/goods_list/index.js
+import request from '../../utils/request'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: "综合",
+        isActive: true
+      },
+      {
+        id: 0,
+        value: "销量",
+        isActive: false
+      },
+      {
+        id: 0,
+        value: "价格",
+        isActive: false
+      }
+    ],
+    goodList:{}
+  },
+  // 请求参数
+  queryParams:{
+    query:'',
+    cid:'',
+    pagenum:1,
+    pagesize:10
+  },
+  // page事件写在于data同级里
+  hadletabItemChange(e) {
+    // console.log(e);
+    // 拿到index去修改isActive的值
+    const { index } = e.detail;
+    let { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false)
+    this.setData({
+      tabs
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // console.log(options);
+    this.queryParams.cid = options.cid;
+    // 发请求
+    request({
+      url:'/goods/search',
+      data:this.queryParams
+    }).then(res=> {
+      console.log(res);
+     const {message} =res.data 
+     this.setData({
+      goodList:message
+     })
+    })
   },
 
   /**
