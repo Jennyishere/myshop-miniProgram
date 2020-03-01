78a76d28f7f7f6ce8d53d0c3574ed40aada750bc
Jeny <853297695@qq.com>
2020-03-02 00:12:49 +0800

    feat:添加地址的功能实现

--- pages/Cart/index.js
+++ pages/Cart/index.js
@@ -5,62 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: {}
   },
+  // 点击获取地址
+  handleChooseAddress() {
 
+    wx.getSetting({
+      success: (res) => {
+        console.log(res);
+        const scopeAddress = res.authSetting['scope.address'];
+        if (scopeAddress === false) {
+          // false说明点击了取消 则需要引导用户再次打开设置
+          wx.openSetting({
+            success: (res) => {
+              console.log(res);
+            },
+          })
+        }
+        // 点过或者从未点开过 
+        wx.chooseAddress({
+          success: (res => {
+            // 把地址存在本地
+            res.all = res.provinceName+res.cityName+res.countyName+res.detailInfo
+            wx.setStorageSync('address', res)
+          })
+        })
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onShow: function (options) {
+    const address = wx.getStorageSync('address')
+    this.setData({
+      address
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
