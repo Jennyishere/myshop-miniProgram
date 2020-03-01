f681fa587ff32d4725e1d606ad3128d01bb80b03
Jeny <853297695@qq.com>
2020-03-01 20:50:37 +0800

    feat: 底部工具栏静态完成 图片放大功能完成 差点击功能

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -8,7 +8,17 @@ Page({
   data: {
     goodDetails: {},
   },
-
+  // 把图片存出来
+  goodsPics:[],
+// 实现点击预览大图
+handlePreviewImg(e) {
+  const {index} = e.currentTarget.dataset
+  const urls = this.goodsPics.map(v=> v.pics_mid)
+  wx.previewImage({
+    current:urls[index],
+    urls: urls,
+  })
+},
   /**
    * 生命周期函数--监听页面加载
    */
@@ -23,13 +33,13 @@ Page({
     }).then(res => {
       console.log(res);
       const { message } = res.data
+      this.goodsPics = message.pics
       this.setData({
         goodDetails: {
           goods_name: message.goods_name,
           goods_price: message.goods_price,
           goods_introduce: message.goods_introduce,
           pics: message.pics,
-
         }
       })
 
