71d955b3a3a7fcf6f1761384e248daae825657a3
Jeny <853297695@qq.com>
2020-03-01 21:46:53 +0800

    feat:底部功能完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -9,15 +9,45 @@ Page({
     goodDetails: {},
   },
   // 把图片存出来
-  goodsPics:[],
+  goods:[],
 // 实现点击预览大图
 handlePreviewImg(e) {
   const {index} = e.currentTarget.dataset
-  const urls = this.goodsPics.map(v=> v.pics_mid)
+  const urls = this.goods.pics.map(v=> v.pics_mid)
   wx.previewImage({
     current:urls[index],
     urls: urls,
   })
+},
+
+// 加入购物车
+/** 
+ * 步骤：1、先判断当前商品是否存在哦于购物车
+ * 2、已经存在就++，修改数量，再存回本地
+ * 3、不存在于购物车的新商品，给数组添加一个新的元素，再存回去
+ * 4、给用户提示
+*/
+addCart() {
+// 判断本地的数据 不存在就给一个空数组
+let cart = wx.getStorageSync('cart')||[]
+// 找到本地的商品的index对比是否存在
+let index = cart.findIndex(v=>v.goods_id === this.goods.goods_id)
+if(index===-1) {
+  // 不存在 push进去 并把数量设置为1
+  this.goods.num=1;
+cart.push(this.goods)
+}else {
+  // 存在 直接找到该商品 数量加1
+  cart[index].num++;
+}
+// 存回缓存
+wx.setStorageSync('cart', cart);
+// 弹窗提醒用户
+wx.showToast({
+  title: '加入成功',
+  mask:true, //防止手抖
+  icon:'success'
+})
 },
   /**
    * 生命周期函数--监听页面加载
@@ -33,7 +63,7 @@ handlePreviewImg(e) {
     }).then(res => {
       console.log(res);
       const { message } = res.data
-      this.goodsPics = message.pics
+      this.goods = message
       this.setData({
         goodDetails: {
           goods_name: message.goods_name,
